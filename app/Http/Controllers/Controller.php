83e0d02af0abbabf1dbf0a1acc1bd925d016a702
TrungTH <hi@trung.me>
2021-05-21 18:35:45 +0700

    :zap:

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function response()
+    {
+        return [
+            'status' => 200,
+            'message' => 'Sucess'
+        ];
+    }
 }
